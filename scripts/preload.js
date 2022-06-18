9e54e45236dca1405667e08da7fbfcbc35488fbd
onlue <1194900@mtp.by>
2022-06-18 11:45:10 +0300

    final result

---
+++ scripts/preload.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.querySelector('.preloader').classList.add("preloader-remove");
+};
+
+$(window).on('load', function () {
+    $('.preloader1').addClass("preloader-remove");     
+});
